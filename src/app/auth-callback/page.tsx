77c97d2aa6070e901de3f9134d95575164d50a5e
xuniorss <fortunatojunior2020@gmail.com>
2023-10-01 16:16:17 -0300

    feat: tRPC Setup

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,10 +1,17 @@
 'use client'
 
 import { useRouter, useSearchParams } from 'next/navigation'
+import { trpc } from '../_trpc/client'
 
 export default function AuthCallbackPage() {
 	const router = useRouter()
 	const searchParams = useSearchParams()
 
 	const origin = searchParams.get('origin')
+
+	const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+		onSuccess: ({ success }) => {
+			if (success) router.push(origin ? `/${origin}` : '/dashboard')
+		},
+	})
 }
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,8 +1,27 @@
 'use client'
 
-import { QueryClient } from '@tanstack/react-query'
-import { useState } from 'react'
+import { trpc } from '@/app/_trpc/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { httpBatchLink } from '@trpc/client'
+import { PropsWithChildren, useState } from 'react'
 
-export const Providers = () => {
+export const Providers = ({ children }: PropsWithChildren) => {
 	const [queryClient] = useState(() => new QueryClient())
+	const [trpcClient] = useState(() =>
+		trpc.createClient({
+			links: [
+				httpBatchLink({
+					url: 'http://localhost:3000/api/trpc',
+				}),
+			],
+		}),
+	)
+
+	return (
+		<trpc.Provider client={trpcClient} queryClient={queryClient}>
+			<QueryClientProvider client={queryClient}>
+				{children}
+			</QueryClientProvider>
+		</trpc.Provider>
+	)
 }
